3391d6c2ba549ddbd6dd55b790ec63e06385da2f
Jared Black <j.black16@icloud.com>
2023-08-20 14:33:27 -0500

    Updated Commit

---
+++ Core/MTDataAccess/Models/ArtistModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTDataAccess.Models
+{
+    public class Artist
+    {
+        public int artistID { get; set; }
+        public DateTime DateCreation { get; set; }
+        public string title { get; set; }   
+        public string biography { get; set; }
+        public string imageURL { get; set; }
+        public string heroURL { get; set; }
+
+    }
+
+
+}
